0db5b8323fe412435bc08ca266930bea7d2cbf6c
Tobias Placht <tobias.placht@qaware.de>
2016-10-06 14:05:27 +0200

    Added simple unit test to zwitscher-edge. Part of #20.

--- zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
+++ zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
@@ -25,17 +25,28 @@ package de.qaware.cloud.nativ.zwitscher.edge;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestPropertySource("classpath:/application-test.properties")
+@DirtiesContext
+@Configuration
 public class ZwitscherEdgeApplicationTests {
 
+    public static final int DEFINED_CONFIG_SERVER_PORT = 8765;
+
+    @LocalServerPort
+    private int configServerPort;
+
     @Test
-    public void contextLoads() {
+    public void servletContainerIsRunningOnDefinedPort() {
+        assertTrue("servletContainer running on wrong port: ", this.configServerPort == DEFINED_CONFIG_SERVER_PORT);
     }
 
 }
