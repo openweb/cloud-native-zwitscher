73fda418ad23e07cdd157b26350d1d06c1c87843
Tobias Placht <tobias.placht@qaware.de>
2017-02-16 09:13:08 +0100

    Disable DockerCompose inside integration tests

--- zwitscher-eureka/src/integrationTest/java/de/qaware/cloud/nativ/zwitscher/eureka/ZwitscherEurekaIntegrationTests.java
+++ zwitscher-eureka/src/integrationTest/java/de/qaware/cloud/nativ/zwitscher/eureka/ZwitscherEurekaIntegrationTests.java
@@ -24,10 +24,6 @@
 
 package de.qaware.cloud.nativ.zwitscher.eureka;
 
-import com.palantir.docker.compose.DockerComposeRule;
-import com.palantir.docker.compose.connection.DockerPort;
-import com.palantir.docker.compose.connection.waiting.HealthChecks;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,31 +43,12 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class ZwitscherEurekaIntegrationTests {
 
-    private static final String SERVICE_NAME = "zwitschereureka";
-
-    private static final int SERVICE_PORT = 8761;
-
-    private static final String DOCKER_PORT_FORMAT = "http://$HOST:$EXTERNAL_PORT";
-
-    private static final String DOCKER_COMPOSE_FILE_LOCATION = "src/integrationTest/resources/docker-compose.yml";
-
-    private static final String LOG_OUTPUT_DIRECTORY = "build/dockerLogs/dockerComposeRuleTest";
-
     private static final String KEYWORD = "eureka";
-
-    @ClassRule
-    public static DockerComposeRule docker = DockerComposeRule.builder()
-            .file(DOCKER_COMPOSE_FILE_LOCATION)
-            .waitingForService(SERVICE_NAME, HealthChecks.toHaveAllPortsOpen())
-            .saveLogsTo(LOG_OUTPUT_DIRECTORY)
-            .build();
 
     @Test
     public void eurekaServerIsAvailable() throws Exception {
-        DockerPort dockerPort = docker.containers().container(SERVICE_NAME).port(SERVICE_PORT);
-        String url = dockerPort.inFormat(DOCKER_PORT_FORMAT);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8761", String.class);
         assertNotNull("responseEntity is null", responseEntity);
         assertEquals("wrong status code", HttpStatus.OK, responseEntity.getStatusCode());
         assertTrue("eureka is not running", responseEntity.getBody().contains(KEYWORD));
